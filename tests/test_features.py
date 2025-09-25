a905d6ae3ad2c1199bf6928df2a2d8e3fab8e688
Nadja <nadjadaffron@gmail.com>
2025-09-25 10:32:13 -0400

    Add files via upload

---
+++ tests/test_features.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from neuroadaptive.features import extract_features, feature_vector
+
+
+def test_extract_features_shapes():
+    samples = np.random.randn(4, 512)
+    feats = extract_features(samples, fs=256)
+    vec = feature_vector(feats)
+    assert len(feats) == vec.shape[0]
+    assert vec.ndim == 1
